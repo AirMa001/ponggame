57ce03564a19542a3334855b7be90ed1aa9b42b8
Emmanuel <incrediblemayo@gmail.com>
2025-06-19 20:27:34 +0100

    added html

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path'); // add this
 const router = express.Router();
 
+
+
 router.get('/', (req, res) => {
-    res.send("Multiplayer Pong server is running");
+  res.sendFile(path.join(__dirname, '../test-client.html')); // use path.join to resolve HTML file
 });
 
 module.exports = router;
